fb99b75b03dcc11cc6c077b3f89e2489030553b9
Marty <zamorasxyron424@gmail.com>
2024-01-30 16:20:16 +0800

    dakug otin

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,32 +1,16 @@
 //projects start
 const projects = [
+  
   {
-    name: "Booking.com clone",
+    name: "Im a famous tiktoker",
     description:
-      "A clone of booking.com which is built using Nextjs 14 and Oxylabs webscraping API to scrape data from the official booking.com website and show as results of our queries",
+      "I got 3k views on tiktok",
     stack: ["React", "NodeJS", "Express", "MongoDB"],
-    sourceCode: "https://github.com/randilt/booking.com-clone-nextjs14",
-    img: "/bookingcom.jpg",
-    preview: "https://bookingcomnext14.netlify.app/",
+    sourceCode: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley",
+    img: "/3k-views-on-tiktok.png",
+    preview: "https://www.tiktok.com/@1ndonlyash/video/7327540884986154246",
   },
-  {
-    name: "Cyberscourge Hub",
-    description:
-      "An open source ultra fast markdown blog app built using Hugo and deployed on Netlify. The theme of this app is 100% custom built by me. The theme is also open source and can be found on my github profile. And I regularly write articles on this blog.",
-    stack: ["React", "NodeJS", "Express", "MongoDB"],
-    sourceCode: "https://github.com/randilt/cyberscourge-hub",
-    img: "/cyberscourge.jpg",
-    preview: "https://rtharusha.tech",
-  },
-  {
-    name: "Jotion (Notion Clone)",
-    description:
-      "A clone of the popular note taking app Notion. This app is built using Nextjs 13 and Tailwind CSS. It uses clerk for authentication and convex for its real time database. The project is not yet complete ",
-    stack: ["React", "NodeJS", "Express", "MongoDB"],
-    sourceCode: "https://github.com/randilt/notion-clone",
-    img: "/jotion.jpg",
-    preview: "#",
-  },
+ 
 ];
 
 //projects end
