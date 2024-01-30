a24663b851c47ea3cb1c86628a56d86ca25b7a6b
Marty <zamorasxyron424@gmail.com>
2024-01-30 16:20:56 +0800

    bfdh

--- src/content/skills.js
+++ src/content/skills.js
@@ -4,16 +4,10 @@ const skills = [
   "js.svg",
   "html.svg",
   "css.svg",
-  "framer-motion.svg",
   "nodejs.svg",
-  "express.svg",
-  "python.svg",
   "bootstrap.svg",
   "git.svg",
-  "github.svg",
-  "mongodb.svg",
-  "tailwind.svg",
-  "linux.svg",
+  "github.svg",,
 ]; // Add or remove skills here, make sure to include the image respective to the skill inside the public/skills folder and use the correct file extension
 
 export default skills;
